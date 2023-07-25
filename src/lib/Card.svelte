3325d683fe40aee3204267a9a34dc4834c3ccb80
bear <isadelski@gmail.com>
2023-07-25 01:16:00 -0400

    first commit

---
+++ src/lib/Card.svelte
@@ -0,0 +1,85 @@
+<script>
+    export let title;
+    export let description;
+    export let gitLink;
+    export let techStack;
+    import DiGithubBadge from 'svelte-icons/di/DiGithubBadge.svelte';
+</script>
+
+<main>
+    <div class="container">
+        <div class="info">
+            <h2> {title}</h2>
+                <div class="gitIcon">
+                    <a class="link" href={gitLink} target="_blank"><DiGithubBadge /></a>
+                </div>
+            <p>{description}</p>
+            <div class="techWrapper">
+                <p class="tech">{techStack}</p>
+            </div>
+            
+        </div>
+    </div>
+</main>
+
+<style>
+    .info{
+    display: flex;
+    flex-wrap: wrap;
+    padding-right: 3px;
+    padding-left: 3px;
+    margin-bottom: 5px;
+    color:#8cade6;
+    margin-top: 5px;
+    margin-left: 15px; 
+    
+}
+p{
+    color: white;
+    margin: 0;
+    padding-bottom: 10px;
+    font-size: 15px;
+}
+h2{
+    margin-top: 5px;
+    margin-bottom: 15px;
+}
+.container {
+    max-width: 390px;
+    margin-left: auto;
+    margin-right: auto;
+    min-width: 300px;
+    background-color:#111;
+    margin-left: 0px;
+    margin-top: 25px;
+    margin-bottom: 10px;
+    border-radius: 20px;
+    box-shadow: 0px 0px 10px 10px #0d0d0d88;
+    border-color: #bbb;
+    border-width: 0.1px; 
+    border-style: solid;
+}
+.gitIcon{
+    height: 27px;
+    width: 27px;
+    margin-left: auto;
+    margin-right: 17px;
+    margin-top: 6px;
+    margin-bottom: 0;
+}
+.link{
+    color: #8cade6;
+}
+.gitIcon:hover {
+    filter: brightness(120%);
+    transition: filter 0.3s ease; 
+}
+.tech{
+    font-size: 12px;
+    color: #bbb;
+}
+.techWrapper{
+    padding-top: 10px;
+}
+
+</style>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,111 @@
+<script>
+    import Card from "$lib/Card.svelte"
+</script>
+
+<svelte:head>
+  <link
+    href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@500&family=Roboto+Serif:wght@500&family=Roboto+Slab:wght@500&family=Roboto:ital,wght@0,400;0,500;1,400&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+<main>
+    <body>
+        <div class="container">
+            <div class ="middle">
+                <div class="content">
+                    <p>
+                        <a href="/">home</a>
+                    </p>
+                    <h1>eryks</h1>
+                    <div class="word-container">
+                        <div class="typed-out">projects</div>
+                    </div> 
+                    <Card 
+                    title="Statify" 
+                    description="Statify is an application that displays a spotify users top tracks and artists over different timeframes, allowing users to get a better understanding of their music history" 
+                    gitLink="https://github.com/surgicalbear/spotifystats" 
+                    techStack="Next.js React JavaScript TypeScript Shadcn"
+                    />
+                </div>
+            </div>
+        </div>
+    </body>
+</main>
+
+<style>
+
+body {
+    font-family: ibm plex mono,monospace;
+    color: #fff;
+    background-color: #111;
+    font-size: 14px;
+    padding: 20px;
+    overflow-y: scroll;
+}
+
+* {
+    box-sizing: border-box;
+}
+.container {
+    display: table;
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding-top: 50px;
+    padding-bottom: 50px;
+    height: 100%;
+    width: 100%;
+    background-color: #111;
+  }
+
+.middle {
+    display: table-cell;
+    vertical-align: middle;
+}
+.content {
+    max-width: 390px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 40%;
+    min-width: 300px;
+    background-color: #111;
+    border-top: 2px solid #fff;
+    border-bottom: 2px solid #fff;
+    padding-bottom: 15px;
+}
+
+h1{
+    margin: 0;
+    font-size: 28px;
+    font-weight: 700;
+    display: inline-block;
+    vertical-align: middle;
+}
+
+.word-container{
+    display: inline-block;
+    vertical-align: middle;
+}
+.typed-out{
+    color:#8cade6;
+    font-size: 28px;
+    font-weight: 700;
+    width:100%;
+    overflow: hidden;
+    white-space: nowrap;
+    animation: typing 1.4s;
+}
+
+@keyframes typing {
+    from { width: 0}
+    to {width: 100%}
+}
+
+a{
+    color: #8cade6;
+}
+a:hover{
+    color: white;
+}
+
+</style>
